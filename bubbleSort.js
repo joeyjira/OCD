6b22e646247d60b5c9cf9207e8c82f604b83bd2a
Joey Jirasevijinda <joey.jira@gmail.com>
2017-11-03 23:00:23 -0700

    bubble sort started

---
+++ bubbleSort.js
@@ -0,0 +1,9 @@
+function bubbleSort() {
+    let tileStrip = document.querySelectorAll('tile-strip')
+
+    for (let i = 0; i < tileStrip.length; i++) {
+        console.log('check this out', tileStrip[i])
+    }
+}
+
+bubbleSort();
